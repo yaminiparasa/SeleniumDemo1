7457d9118cb59c16e400c020deac941dc25fc1ef
yaminiparasa <165988038+yaminiparasa@users.noreply.github.com>
2024-04-04 12:39:58 +0530

    Update TC_03.java
    updated testcase 3

--- Demo1/src/com/selenium/demo/TC_03.java
+++ Demo1/src/com/selenium/demo/TC_03.java
@@ -1,9 +1,9 @@
 package com.selenium.demo;
 
-public class TC_1 {
+public class TC_03 {
 
 	public static void main(String[] args) {
-		System.out.println("testcase 1");
+		System.out.println("testcase 3");
 		System.out.println("hello");
 		
 	}
